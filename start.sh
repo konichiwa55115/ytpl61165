fce1355b646ac9b9a91fbc8bba5cce412dc9ae1d
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-01-29 03:46:00 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ git clone https://github.com/konichiwa55115/ytdl51155115sdasd /LazyDeveloper
 cd /LazyDeveloper
 pip3 install -r requirements.txt
 echo "Starting Bot...."
-python3 -m  bot
+python3 -m bot
