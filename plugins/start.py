4d336cac5531c0917dd9b09d154fc53ad0d575ee
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-01-29 03:09:14 +0200

    Add files via upload

---
+++ plugins/start.py
@@ -0,0 +1,15 @@
+from pyrogram import Client, filters, StopPropagation
+from pyrogram.types.bots_and_keyboards.inline_keyboard_button import InlineKeyboardButton
+from pyrogram.types.bots_and_keyboards.inline_keyboard_markup import InlineKeyboardMarkup
+
+@Client.on_message(filters.command(["start"]), group=-2)
+async def start(client, message):
+    # return
+    joinButton = InlineKeyboardMarkup([
+        [InlineKeyboardButton("Channel", url="https://t.me/aryan_bots")],
+        [InlineKeyboardButton(
+            "Report Bugs 😊", url="https://t.me/aryanvikash")]
+    ])
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    await message.reply_text(welcomed, reply_markup=joinButton)
+    raise StopPropagation
