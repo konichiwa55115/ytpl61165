6eced34fe5eb237b4e103e7eca2d378688c25b57
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-01-29 03:04:18 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+import  os
+
+BOT_TOKEN = "6165770459:AAFEgNaxaLJDGJGw6NIstrwtyuvw3EYebFs"
+APP_ID = int(17983098)
+API_HASH = "ee28199396e0925f1f44d945ac174f64"
+
+youtube_next_fetch = 0  # time in minute
+
+
+EDIT_TIME = 5
